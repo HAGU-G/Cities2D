b19e36ced5e62c803f456ef8fea423f4e105f1bb
조병민 <jsjbm@naver.com>
2024-02-14 16:38:39 +0900

    SFML-GM 테스트 실패 오류 수정 중

--- main.cpp
+++ main.cpp
@@ -1,9 +1,65 @@
 ﻿#include <iostream>
 #include "pch.h"
 
+
+class testObj: public sfgm::Object
+{
+public:
+    testObj(std::string str)
+        :Object(str)
+    {
+
+    }
+    sf::Sprite sprite;
+
+    void Draww(sf::RenderWindow& wn)
+    {
+        wn.draw(sprite);
+    }
+protected:
+    void Update(float timeDelta, float timeScale) override
+    {
+
+    }
+    void Draw() override
+    {
+
+    }
+    void Init() override
+    {
+        sf::Texture tex;
+        tex.loadFromFile("graphics/tree");
+        sprite.setTexture(tex);
+    }
+};
+
+class testScene : public sfgm::Scene
+{
+protected:
+    void Init() override
+    {
+
+    }
+    void Update(float timeDelta, float timeScale) override
+    {
+
+    }
+    void Draw() override
+    {
+
+    }
+};
+
+
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(100, 100), "test", sf::Style::Default);
+    
+    testScene dd;
+    dd.AddObject(std::shared_ptr<testObj>(new testObj("name")));
+
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -12,6 +68,18 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+        dd.pre_Init();
+        window.clear();
+        dd.pre_Update(1.f, 1.f);
+        dd.pre_Draw();
+        auto s = dd.GetObjects().find("name");
+        for (auto& i : s->second)
+        {
+            dynamic_cast<testObj*>(i.get())->Draww(window);
+        }
         window.display();
     }
 }
+
+
+
